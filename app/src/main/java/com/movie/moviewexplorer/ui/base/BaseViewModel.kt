7887993a0b3993a8a5b8d1c18ea13f249a3a8ce1
George <george070skv@gmail.com>
2018-10-02 23:55:05 +0300

    Create SearchMovieActivity.

---
+++ app/src/main/java/com/movie/moviewexplorer/ui/base/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.movie.moviewexplorer.ui.base
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/movie/moviewexplorer/ui/models/ParcelableEntities.kt
@@ -0,0 +1,3 @@
+package com.movie.moviewexplorer.ui.models
+
+data class MovieParcelable (val posterUrl: String, val title: String, val year: String)
