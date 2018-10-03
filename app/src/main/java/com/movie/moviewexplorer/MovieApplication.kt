e5dbe681bf8a6687c024d57809d3dc6e97f5fc71
George <george070skv@gmail.com>
2018-10-04 00:28:19 +0300

    Disable multidex

--- app/src/main/java/com/movie/moviewexplorer/MovieApplication.kt
+++ app/src/main/java/com/movie/moviewexplorer/MovieApplication.kt
@@ -1,7 +1,6 @@
 package com.movie.moviewexplorer
 
 import android.app.Application
-import androidx.multidex.MultiDexApplication
 import com.movie.data.di.apiModule
 import com.movie.moviewexplorer.di.implModule
 import com.movie.moviewexplorer.di.schedulersModule
@@ -9,7 +8,7 @@ import com.movie.moviewexplorer.di.useCaseModule
 import com.movie.moviewexplorer.di.viewModelModule
 import org.koin.android.ext.android.startKoin
 
-class MovieApplication : MultiDexApplication() {
+class MovieApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
